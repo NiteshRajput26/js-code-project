7993f4e8a606a6788e0ef066f1409e260adc29fc
NiteshRajput26 <148003383+NiteshRajput26@users.noreply.github.com>
2023-12-16 15:40:05 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,26 @@
+let score = "33abs"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NAN  not a number
+//true => 1 , false => 0
+// null => 0, undefined => NAN
+
+
+let isLoggedIn = "nitesh"
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1 => true , 0 =>false
+// "" => false
+// "Nitesh" => true   
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
