d0a6d118a7f979c637b2e8f754ada38c67f819fa
NiteshRajput26 <148003383+NiteshRajput26@users.noreply.github.com>
2023-12-16 12:52:41 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version 
+
+//alert( 3 + 3) // we are useing nodejs , not browser(console)
+
+console.log(3
+    +
+    3); //code readebility should be high
+
+    console.log("Nitesh")
+    let name = "Nitesh"
+    let age = 18
+    let isLoggedIn = false
+    let state ;
+
+    // number => 2 to power 53
+    // bigint
+    // string =>
+    // boolean
+    // null
+    //undefined
+    //symbol => unique
+
+    console.log(typeof null); //object
+    console.log(typeof undefined);  //undefined
