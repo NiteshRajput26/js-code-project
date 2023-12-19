1f4aacb7aa7a4ade79086d5087a8ef91d5627457
NiteshRajput26 <148003383+NiteshRajput26@users.noreply.github.com>
2023-12-19 20:12:27 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,7 @@
+const name = "nitesh"
+const repoCount = 50
+
+ //console.log(name + repoCount + "Value");   //old methods
+
+console.log(`Hello my name is ${name} and my repo count is $ 
+{repoCount}`);
