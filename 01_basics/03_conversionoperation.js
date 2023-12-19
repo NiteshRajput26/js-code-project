3223b50a0bf978a75ca9646de4da67e33dd2ec15
NiteshRajput26 <148003383+NiteshRajput26@users.noreply.github.com>
2023-12-19 16:08:53 +0000

    Conversion Operation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -15,12 +15,51 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = "nitesh"
 let booleanIsLoggedIn =Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 //1 => true , 0 =>false
 // "" => false
 // "Nitesh" => true   
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *********************OPERATIONS******************
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+
+//************some mathematical operations ******/
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Nitesh"
+let str3 = str1+str2
+console.log(str3);
+
+
+// http://tc39.ec/ecma262.... website for more documentation
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log((3+4) * 5 % 3);
+
+//console.log(true+);   wrong syntax
+console.log(+true);
+
+// prefix and postfix for js mdn ....http:devloper.mozila...
+let gameCounter = 100;
+gameCounter++;
+// ++gameCounter
+console.log(gameCounter);
