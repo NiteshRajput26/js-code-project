9b0f9e51098ea6f675d79cb58d82d51af1847760
NiteshRajput26 <148003383+NiteshRajput26@users.noreply.github.com>
2023-12-24 18:27:49 +0000

    Array

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -15,6 +15,15 @@ const dc_heros = ["superman", "flash", "batman"]
 //  console.log(all_new_heros);
 
 const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
-// 
+// flat ka use multiple sub array ko single array me concatinate krne ke liye krte hai
 const real_another_array = another_array.flat(Infinity)
 console.log(real_another_array);
+
+console.log(Array.isArray("Nitesh"));
+console.log(Array.from("Nitesh"));
+console.log(Array.from({name: "Nitesh"}));
+
+let score1 =100;
+let score2 =200;
+let score3 =300;
+console.log(Array.of(score1,score2,score3));
