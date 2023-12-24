7cdc1d0ffc55761d78db22d8d12ef74cde7403e3
NiteshRajput26 <148003383+NiteshRajput26@users.noreply.github.com>
2023-12-24 19:16:58 +0000

    objects 2

---
+++ 02_basics/04_object.js
@@ -0,0 +1,9 @@
+//singleton
+
+//const tinderUser = new Object{}
+const tinderUser = {}
+tinderUser.id = "123abc"
+tinderUser.name = "samy"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser);
