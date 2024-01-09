d4ced182b6ab10736ffbd3bb08540d68e5b33c29
NiteshRajput26 <148003383+NiteshRajput26@users.noreply.github.com>
2024-01-09 08:25:31 +0000

    control switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,49 @@
+// Basic Syntax
+//1.
+// switch(key){
+//     case value:
+//         break;
+
+//         default:
+//         break;
+
+// }
+
+
+//2. 
+// const month = 5
+
+// switch (month) {   // shift + alt + downarrow use 
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("february");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+
+//     default:
+//         console.log("another month");
+//         break;
+// }
+
+// 3.
+
+const month = "may"
+switch (month) {   // shift + alt + downarrow use 
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("february");
+        break;
+    case "march":
+        console.log("march");
+        break;
+
+    default:
+        console.log("another month");
+        break;
+}
