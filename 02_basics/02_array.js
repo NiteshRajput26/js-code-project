04dfd7232c92ea1bab8ea53be553e71f7a443245
NiteshRajput26 <148003383+NiteshRajput26@users.noreply.github.com>
2024-01-09 07:26:22 +0000

    array

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,18 +1,18 @@
 const marvel_heros = ["thor" , "Ironman" ,"spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-// marvel_heros.push(dc_heros)
-// console.log(marvel_heros);
-// console.log(marvel_heros[3][1]);
+//marvel_heros.push(dc_heros)
+// console.log(marvel_heros);   // o/p:  [ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
+// console.log(marvel_heros[3][1]);   //o/p: flash
 
 // const allHeros = marvel_heros.concat(dc_heros)
-//  console.log(allHeros);
+//  console.log(allHeros);     // o/p: [ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
 
- // spread method kanch ke gilas ke jaise spread ho jayega
-// concate se jyada spread ko prefer krte hai
+ //*********** */ spread method kanch ke gilas ke jaise spread ho jayega
+// ************** concate se jyada spread ko prefer krte hai
 
 //  const all_new_heros = [...marvel_heros, ...dc_heros]
-//  console.log(all_new_heros);
+//  console.log(all_new_heros);    //o/p: [ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
 
 const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
 // flat ka use multiple sub array ko single array me concatinate krne ke liye krte hai
